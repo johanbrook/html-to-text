22dc764337e98e4ac483e056a101409f3493280f
Johan Brook <johan.jvb@gmail.com>
2016-11-29 00:26:47 -0500

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'lookback:html-to-text',
   summary: 'Meteor wrapper',
-  version: '2.1.3_1',
+  version: '2.1.3_2',
   git: 'https://github.com/johanbrook/html-to-text.git',
   documentation: null
 });
